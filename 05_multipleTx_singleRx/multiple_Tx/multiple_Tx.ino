e1d57ac3ecb5047c1d5b690f9fb09eced54d14c3
carlobiermann <carlo.biermann@protonmail.com>
2021-03-07 21:12:12 +0100

    trying multiple Tx single Rx

---
+++ 05_multipleTx_singleRx/multiple_Tx/multiple_Tx.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISnO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND  868E6
+
+unsigned int counter = 0;
+
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+ 
+void setup() {
+  pinMode(16,OUTPUT);
+  pinMode(2,OUTPUT);
+  
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Sender Test");
+  
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  //LoRa.onReceive(cbk);
+//  LoRa.receive();
+  Serial.println("init ok");
+   
+  delay(1500);
+}
+
+void loop() {
+  Serial.println(String(counter));
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.setSpreadingFactor(8);
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
---
+++ 05_multipleTx_singleRx/single_Rx/single_Rx.ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND    868E6
+
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+
+void loraData(){
+  Serial.println(rssi);
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+void setup() {
+
+  
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Receiver Callback");
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);  
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+  Serial.println("init ok");
+     
+  delay(1500);
+}
+
+void loop() {
+  LoRa.setSpreadingFactor(8);
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
+}
