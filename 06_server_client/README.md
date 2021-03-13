a2c5e4ec50b7ca090a7c65cb0bd017a714ce72ac
carlobiermann <carlo.biermann@protonmail.com>
2021-03-13 11:22:17 +0100

    add server client

---
+++ 06_server_client/README.md
@@ -0,0 +1,3 @@
+## Server-Client Connection 
+
+This folder contains the sketches to connect the T-Beam Client with a Server over the local network. 
---
+++ 06_server_client/client/client.ino
@@ -0,0 +1,49 @@
+// Code from: https://techtutorialsx.com/2018/05/17/esp32-arduino-sending-data-with-socket-client/
+
+
+#include <WiFi.h>
+ 
+const char* ssid = "";
+const char* password =  "";
+ 
+const uint16_t port = 8888;
+const char * host = "192.168.XX.XX";
+ 
+void setup()
+{
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("...");
+  }
+ 
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+ 
+}
+ 
+void loop()
+{
+    WiFiClient client;
+ 
+    if (!client.connect(host, port)) {
+ 
+        Serial.println("Connection to host failed");
+ 
+        delay(1000);
+        return;
+    }
+ 
+    Serial.println("Connected to server successful!");
+
+    delay(1000);
+    client.print("Hello from ESP32!");
+ 
+    Serial.println("Disconnecting...");
+    client.stop();
+ 
+    delay(10000);
+}
