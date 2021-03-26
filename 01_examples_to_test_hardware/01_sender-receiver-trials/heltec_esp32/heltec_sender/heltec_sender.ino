6732814360ace87c819de58fd455d0e3a2818e57
Carlo Biermann <39556873+carlobiermann@users.noreply.github.com>
2021-03-26 15:44:15 +0100

    Update heltec_sender.ino

--- 01_examples_to_test_hardware/01_sender-receiver-trials/heltec_esp32/heltec_sender/heltec_sender.ino
+++ 01_examples_to_test_hardware/01_sender-receiver-trials/heltec_esp32/heltec_sender/heltec_sender.ino
@@ -1,4 +1,5 @@
 /* 
+  Code from: https://github.com/HelTecAutomation/Heltec_ESP32/blob/master/examples/LoRa/LoRaSender/LoRaSender.ino
   Basic test program, send date at the BAND you seted.
   
   by Aaron.Lee from HelTec AutoMation, ChengDu, China
